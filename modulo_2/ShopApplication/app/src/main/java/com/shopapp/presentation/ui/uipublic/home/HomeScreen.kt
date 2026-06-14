91455302257c0659256c99051b7d32819fb00173
Slowlyxs <alejandroalarcon851@gmail.com>
2026-06-14 08:40:35 -0500

    ShopApplication hasta avatar

--- modulo_2/ShopApplication/app/src/main/java/com/shopapp/presentation/ui/uipublic/home/HomeScreen.kt
+++ modulo_2/ShopApplication/app/src/main/java/com/shopapp/presentation/ui/uipublic/home/HomeScreen.kt
@@ -2,19 +2,16 @@
 package com.shopapp.presentation.ui.uipublic.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
