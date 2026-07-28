8932e5de521ca31254dd6b19be4eaa93138fff85
Slowlyxs <alejandroalarcon851@gmail.com>
2026-07-27 19:29:37 -0500

    Tarea

---
+++ modulo_3/MyProyect/flutter_mp/mp_08_material3/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Nicolas\\appflutter_u\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Nicolas\\Downloads\\Progra\\programaciion_4_manana_alarcon\\modulo_3\\MyProyect\\flutter_mp\\mp_08_material3" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Nicolas\\appflutter_u\\flutter"
+  "PROJECT_DIR=C:\\Users\\Nicolas\\Downloads\\Progra\\programaciion_4_manana_alarcon\\modulo_3\\MyProyect\\flutter_mp\\mp_08_material3"
+  "FLUTTER_ROOT=C:\\Users\\Nicolas\\appflutter_u\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Nicolas\\Downloads\\Progra\\programaciion_4_manana_alarcon\\modulo_3\\MyProyect\\flutter_mp\\mp_08_material3\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Nicolas\\Downloads\\Progra\\programaciion_4_manana_alarcon\\modulo_3\\MyProyect\\flutter_mp\\mp_08_material3"
+  "FLUTTER_TARGET=C:\\Users\\Nicolas\\Downloads\\Progra\\programaciion_4_manana_alarcon\\modulo_3\\MyProyect\\flutter_mp\\mp_08_material3\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDQuNg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZWU4MGYwOGJiZg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ODM2NzVlZDI3Ng==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4y"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Nicolas\\Downloads\\Progra\\programaciion_4_manana_alarcon\\modulo_3\\MyProyect\\flutter_mp\\mp_08_material3\\.dart_tool\\package_config.json"
+)
